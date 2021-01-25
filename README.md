51e36618362f9015f943cf6ef68ffd9f364745d0
LukeAmos00 <56327020+lilxxxgravy@users.noreply.github.com>
2021-01-25 12:25:09 +0000

    Version 1.0.0

---
+++ README.md
@@ -0,0 +1,2 @@
+# Twitch-Search-Extension
+Adds a search box to bottom of 'following' list on Twitch.tv which filters list to easily find the user you are searching for. (Chrome Browser Extension)
---
+++ contentScript.js
@@ -0,0 +1,119 @@
+const checkSearchBox = () => {
+    const selector = "#sideNav > div > div > \
+    div.side-nav-search-input.tw-border-t.tw-pd-1";
+    const sideNavEndBox = document.querySelector(selector);
+
+    const preExisitingBox = document.getElementById(
+        'side-nav.find-following');
+
+    /* Inserts search box if window size is large enough */
+    if (sideNavEndBox && !preExisitingBox) {
+        insertSearchBox();
+        filterUsers();
+    }
+};
+
+const insertSearchBox = () => {
+
+    /* Select section at bottom of sideNav */
+    const selector = "#sideNav > div > div > \
+    div.side-nav-search-input.tw-border-t.tw-pd-1";
+    const sideNavEndBox = document.querySelector(selector);
+
+    /* Clone search friends box */
+    const searchBoxClone = sideNavEndBox.lastChild.cloneNode(true);
+    sideNavEndBox.insertBefore(
+        searchBoxClone,
+        sideNavEndBox.lastChild);
+
+    /* Add spacing between two search boxes */
+    const seachBox = sideNavEndBox.firstChild;
+    seachBox.style.marginBottom = "10px";
+
+    seachBox.firstChild.setAttribute(
+        "for",
+        "side-nav.find-following");
+
+    /* Change text inside box */
+    const searchBoxInput = seachBox.lastChild.lastChild;
+    searchBoxInput.setAttribute("placeholder", "Search Following");
+
+    searchBoxInput.setAttribute("id", "side-nav.find-following");
+};
+
+const getFollowList = () => {
+
+    /* Expand follow list */
+    let showMore = document.querySelector(
+        'button[data-test-selector="ShowMore"]');
+
+    while (showMore != null) {
+        showMore.click();
+        showMore = document.querySelector(
+            'button[data-test-selector="ShowMore"]');
+    }
+
+    return document.querySelectorAll(
+        'a[data-test-selector="followed-channel"]');
+};
+
+const filterUsers = () => {
+    const followingListNodes = getFollowList();
+
+    /* Case of following no one */
+    if (!followingListNodes.length) return;
+
+    const input = document.getElementById('side-nav.find-following');
+    input.addEventListener('input', () => {
+        if (input.value === "") {
+            sortFollowing();
+            return;
+        }
+
+        Array.from(followingListNodes).map(node => {
+            const searchQuery = input.value.toLowerCase();
+            const channel = node.getAttribute('href').toLowerCase();
+
+            if (channel.includes(searchQuery)) {
+                pushTop(node);
+            }
+        });
+    });
+};
+
+const sortFollowing = () => {
+    const followingList = document.querySelectorAll(
+        'a[data-test-selector="followed-channel"]');
+
+    const sortedNodes = Array.from(followingList).sort((a, b) => {
+        const aNum = parseInt(
+            a.getAttribute('data-a-id').match(/[0-9]+/)[0]);
+        const bNum = parseInt(
+            b.getAttribute('data-a-id').match(/[0-9]+/)[0]);
+        return aNum - bNum;
+    });
+
+    sortedNodes.reverse().map(pushTop);
+};
+
+const pushTop = node => {
+    const followingList = document.querySelector(
+        'div[aria-label="Followed Channels"]').children[1];
+    
+    const root = node.parentElement.parentElement
+        .parentElement;
+
+    followingList.removeChild(root);
+    followingList.insertBefore(
+        root,
+        followingList.firstChild
+    );
+};
+
+checkSearchBox();
+
+/* Makes box reappear when enough room, delay ensures container loads first */
+window.addEventListener('resize', () => setTimeout(checkSearchBox, 1));
+
+/* filterUsers requires Twitch's scripts to have finished loading */
+window.addEventListener("load", () => setTimeout(filterUsers, 10));
